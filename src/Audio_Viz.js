f66b402e34398822a25dcad604e2c727d2d2ca30
Ryan Herrin <herrin_ryan@yahoo.com>
2022-04-12 20:21:59 -0500

    Resize the bars using amplitude

--- src/Audio_Viz.js
+++ src/Audio_Viz.js
@@ -6,6 +6,7 @@ of the line changes with the pitch.
 */
 
 let noise_line = []; // Array of line objects 
+let spkr_img;
 
 // Define area to run program in 
 const width = 1500;
@@ -13,26 +14,49 @@ const height = 800;
 var bkgrnd_color = 0;
 
 // Set defualt line properties used for
-const start_pos = width; 
+const start_pos = width-110; 
 const vert_pos = height / 2;
 const strk_wght = 1.2; //Thickness of the line 
 const space_between_lines = 1; //Thickness of space in between the lines 
 
 // Initial testing vars. Will be replaced later when the sound lib is added
 const line_length = 10; // Default value 
+var mic_multiplier;
+const input_amp = 2; // Amplifies the signal of mic_multiplier 
 const line_color = 255; // Defulat value is White 
+
+// Preload function to load images
+function preload() {
+    spkr_img = loadImage('img/speaker.png');
+}
 
 // Creates the drawing area 
 function setup () {
-    createCanvas(width, height);
+    let cnv = createCanvas(width, height);
+    // Mic Setup
+    cnv.mousePressed(userStartAudio);
+    textAlign(CENTER);
+    mic_in = new p5.AudioIn();
+    mic_freq = new p5.FFT();
+    mic_in.start();
 }
 
 // What displays the lines 
 function draw() {
     background(bkgrnd_color);
+
+    // Mic interation
+    text('tap to start', width/2, 20);
+    mic_multiplier = mic_in.getLevel();
+    //console.log(mic_in_level);
+
+    // Add image of speaker 
+    image(spkr_img, width-110, (height/2)-65), 20, 20;
+
     // Create new line to add to the array 
     noise_line.push(new AudioLine());
-
+    
+    // Adjust x-axis of each line and re-display. This creates the motion 
     for (let lyne = 0; lyne < noise_line.length; lyne++){
         noise_line[lyne].move();
         noise_line[lyne].display();
@@ -51,7 +75,7 @@ class AudioLine {
     constructor() {
         this.x_pos = start_pos;
         this.y_pos = vert_pos; // Will be dtermined by audio input 
-        this.lngth = line_length;
+        this.lngth = line_length + ((mic_multiplier * input_amp) * height);
         this.thickness = strk_wght;
         this.color = line_color; // Will be dtermined by audio input
     }
